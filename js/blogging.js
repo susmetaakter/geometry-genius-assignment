a2d702d62766bacc0f9460554c13ed0c7b760cf9
Susmeta Akter Mim <susmetaaktermim@gmail.com>
2023-02-18 22:20:51 +0600

    calculation details

---
+++ js/blogging.js
@@ -0,0 +1,3 @@
+ document.getElementById('btn-blog').addEventListener('click', function(){
+     window.location.href = 'blog.html';
+ })
---
+++ js/index.js
@@ -0,0 +1,6 @@
+// for first card
+document.getElementById('triangle-btn').addEventListener('click',function(){
+    // console.log('sub');
+    const triangleCard = document.getElementById('triangle-card');
+    console.log(triangleCard.innerText);
+})
