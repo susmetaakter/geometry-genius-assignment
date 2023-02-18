dd00b3fbd81c025c20744a4b70d41a5e3d33d53f
Susmeta Akter Mim <susmetaaktermim@gmail.com>
2023-02-19 00:04:48 +0600

    second card added

--- js/index.js
+++ js/index.js
@@ -10,3 +10,13 @@ document.getElementById('first-card').addEventListener('click', function(){
     const areaOfShape = 0.5 * parseFloat(valueOfb) * parseFloat(valueOfh);
     displayData(shapeName, areaOfShape.toFixed(2));
 });
+//for second card
+document.getElementById('second-card').addEventListener('click', function(){
+    serial += 1;
+    const shapeName = document.getElementById('rectangle').innerText;
+    const valueOfw = document.getElementById('value-w',).value;
+    //const valueOfb = parseFloat(valueOfB);
+    const valueOfl = document.getElementById('value-l',).value;
+    const areaOfShape = parseFloat(valueOfw) * parseFloat(valueOfl);
+    displayData(shapeName, areaOfShape.toFixed(2));
+});
