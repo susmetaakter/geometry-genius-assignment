09d43350f4d888a257430fff90cd8f37890e3639
Susmeta Akter Mim <susmetaaktermim@gmail.com>
2023-02-18 22:35:39 +0600

    first card added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-// for first card
-document.getElementById('triangle-btn').addEventListener('click',function(){
-    // console.log('sub');
-    const triangleCard = document.getElementById('triangle-card');
-    console.log(triangleCard.innerText);
-})
+
+let serial= 0;
+//for 1st card
+document.getElementById('first-card').addEventListener('click', function(){
+    serial += 1;
+    const shapeName = document.getElementById('triangle').innerText;
+    const valueOfb = document.getElementById('value-b',).value;
+    //const valueOfb = parseFloat(valueOfB);
+    const valueOfh = document.getElementById('value-h',).value;
+    const areaOfShape = 0.5 * parseFloat(valueOfb) * parseFloat(valueOfh);
+    displayData(shapeName, areaOfShape.toFixed(2));
+});
